9cee6622943637226b7362ad70639e6915c71dd6
devyako <yakhyoismoildjonov@gmail.com>
2020-04-19 13:08:42 +0500

    deleting ing

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -8,7 +8,7 @@ const IngredientForm = React.memo((props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({ title: enteredTitle, amount: enteredAmount });
   };
 
   return (
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
+import IngredienList from "./IngredientList";
+import IngredientForm from "./IngredientForm";
+import Search from "./Search";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+const Ingredients = () => {
+  const [userIngredients, setUserIngredients] = useState([]);
 
-function Ingredients() {
+  const addIngredientHandler = (ingredient) => {
+    setUserIngredients((prevIngredient) => [
+      ...prevIngredient,
+      { id: Math.random().toString(), ...ingredient },
+    ]);
+  };
+
+  const deleteIngredientHandler = (id) => {
+    setUserIngredients((prevIngredient) =>
+      prevIngredient.filter((ingredient) => ingredient.id !== id)
+    );
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredienList
+          ingredients={userIngredients}
+          onRemoveItem={deleteIngredientHandler}
+        />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
